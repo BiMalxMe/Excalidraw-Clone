12a2bf04481babba76f264ac3499236271c0b2da
BiMalxMe <journeyxbimal@gmail.com>
2025-03-14 13:42:20 +0545

    addedSignupEndpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,71 +1,86 @@
-
 import express from "express";
 import { middleware } from "./middleware";
-import {CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types"
-import jwt from "jsonwebtoken"
+import {
+  CreateRoomSchema,
+  CreateUserSchema,
+  SigninSchema,
+} from "@repo/common/types";
+import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
-import { prismaClient } from "@repo/db/client"
-import { Request,Response } from "express";
+import { prismaClient } from "@repo/db/client";
+import e from "express";
 const app = express();
+app.use(express.json());
 
-app.post("/signup",async(req:Request,res:Response) => {
-    const payload = req.body;
-    const data = CreateUserSchema.safeParse(payload);
-    if (!payload || !payload.username || !payload.password || !payload.name) {
-        return res.status(400).json({ message: "Missing required fields" });
-      }
-
-    if(data.success){
-        res.json({
-            message : "Incorrect Options"
-        })
-        return;
-    }
-    
-    await prismaClient.user.create({
+app.post("/signup", async (req, res) => {
+  const payload = req.body;
+  const parsedData = CreateUserSchema.safeParse(payload);
+  if (!parsedData.success) {
+    res.json({
+      message: "Incorrect Options",
+    });
+    return;
+  }
+  const existingUser = prismaClient.user.findUnique({
+    where: {
+      email: parsedData.data.email,
+    },
+  });
+  if (!existingUser) {
+    try {
+      await prismaClient.user.create({
         data: {
-          name : payload.name,
-          username : payload?.username ?? "Def",
-          password : payload.password,
-        }
+          email: parsedData.data?.email,
+          password: parsedData.data?.password,
+          name: parsedData.data?.password,
+        },
       });
-      
 
-    //db call
-    res.json({
-        userId : "123"
-    })
-})
-app.post("/signin",(req,res) => {
-    const data = SigninSchema.safeParse(req.body);
-    if(!data.success){
-        res.json({
-            message : "Incorrect Options"
-        })
-        return;
+      //db call
+      res.json({
+        message: "User Signed Up",
+      });
+    } catch (error) {
+      message: "An Error Occurred";
     }
-    const userId = 1;
-    const token = jwt.sign({
-        userId
-    },JWT_SECRET)
-
+  }
+  res.json({
+    message: "An Error Occurred",
+  });
+});
+app.post("/signin", (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+  if (!data.success) {
     res.json({
-        token
-    })
-})
-app.post("/room",middleware,(req,res) => {
-    const data = CreateRoomSchema.safeParse(req.body);
-    if(!data.success){
-        res.json({
-            message : "Incorrect Inpputss"
-        })
-        return
-    }
-    //Now the database calls
+      message: "Incorrect Options",
+    });
+    return;
+  }
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
 
+  res.json({
+    token,
+  });
+});
+app.post("/room", middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
     res.json({
-        roomId : 123
-    })
-})
+      message: "Incorrect Inpputss",
+    });
+    return;
+  }
+  //Now the database calls
+
+  res.json({
+    roomId: 123,
+  });
+});
 
 app.listen(3000);
