aa014ef43025b96f01f85363a6cb47f580a1d7af
BiMalxMe <journeyxbimal@gmail.com>
2025-03-14 13:22:43 +0545

    Some Database debugs

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -4,17 +4,32 @@ import { middleware } from "./middleware";
 import {CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types"
 import jwt from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/backend-common/config";
-
+import { prismaClient } from "@repo/db/client"
+import { Request,Response } from "express";
 const app = express();
 
-app.post("/signup",(req,res) => {
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/signup",async(req:Request,res:Response) => {
+    const payload = req.body;
+    const data = CreateUserSchema.safeParse(payload);
+    if (!payload || !payload.username || !payload.password || !payload.name) {
+        return res.status(400).json({ message: "Missing required fields" });
+      }
+
+    if(data.success){
         res.json({
             message : "Incorrect Options"
         })
         return;
     }
+    
+    await prismaClient.user.create({
+        data: {
+          name : payload.name,
+          username : payload?.username ?? "Def",
+          password : payload.password,
+        }
+      });
+      
 
     //db call
     res.json({
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Response, Request } from "express";
 import jwt from "jsonwebtoken";
 import{JWT_SECRET} from "@repo/backend-common/config"
+
+import { prismaClient } from "@repo/db/client";
+
 export const middleware = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers["authorization"] ?? "";
 
