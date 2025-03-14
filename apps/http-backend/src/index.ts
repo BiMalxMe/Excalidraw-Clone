77ae251064345510607efa0d1039e486812b0895
BiMalxMe <journeyxbimal@gmail.com>
2025-03-14 11:14:41 +0545

    AddSomeRoutesAndCommonPackages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,16 +1,56 @@
 
 import express from "express";
+import { middleware } from "./middleware";
+import {CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types"
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 const app = express();
 
 app.post("/signup",(req,res) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "Incorrect Options"
+        })
+        return;
+    }
 
+    //db call
+    res.json({
+        userId : "123"
+    })
 })
 app.post("/signin",(req,res) => {
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "Incorrect Options"
+        })
+        return;
+    }
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET)
 
+    res.json({
+        token
+    })
 })
-app.post("/room",(req,res) => {
+app.post("/room",middleware,(req,res) => {
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "Incorrect Inpputss"
+        })
+        return
+    }
+    //Now the database calls
 
+    res.json({
+        roomId : 123
+    })
 })
 
 app.listen(3000);
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,15 +1,16 @@
-import { NextFunction,Response ,Request} from "express"
-import  Jwt  from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { NextFunction, Response, Request } from "express";
+import jwt from "jsonwebtoken";
+import{JWT_SECRET} from "@repo/backend-common/config"
+export const middleware = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers["authorization"] ?? "";
 
-export const middleware = (req : Request, res :Response, next : NextFunction) => {
-    const token = req.headers["authorization"];
+  const decoded = jwt.verify(token, JWT_SECRET);
 
-    const decoded = Jwt.verify(token,JWT_SECRET);
-
-    if(decoded){
-            req.userId = decoded.userId;
-    }else{
-        res.json({msg:"Unauthorized"})
-    }
-}
+  if (decoded) {
+    // @ts-ignore
+    req.userId = decoded.userId;
+    next()
+  } else {
+    res.json({ msg: "Unauthorized" });
+  }
+};
